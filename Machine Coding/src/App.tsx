d562b01ecd8daea8053e2408147212cfc814f465
Yash Raj <yashrj130@gmail.com>
2025-06-03 14:10:42 +0530

    Star rating component

--- Machine Coding/src/App.tsx
+++ Machine Coding/src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css';
-import DebouncedSearch from './components/DebouncedSearch/DebouncedSearch';
+import "./App.css";
+import DebouncedSearch from "./components/DebouncedSearch/DebouncedSearch";
+import Rating from "./components/Rating/Rating";
 
 function App() {
-
   return (
     <>
       <DebouncedSearch />
+      <Rating />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
